894a62b0103eff20dd8beb4893c5e859ce1bec01
Ahmet Korkmaz <ahmetkorkmaz3453@gmail.com>
2021-12-11 19:56:09 +0300

    Fix steps count in resource

--- app/Http/Resources/ProjectCaseResource.php
+++ app/Http/Resources/ProjectCaseResource.php
@@ -17,7 +17,7 @@ class ProjectCaseResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'order_no' => $this->order_no,
-            'steps_count' => $this->when(isset($this->steps_count), $this->steps_count),
+            'steps_count' => $this->steps_count ?? 0,
             'project_id' => $this->project_id,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
