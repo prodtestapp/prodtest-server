f227d897ac8f4bad50f1fced51612dbe4ce31a27
Ahmet Korkmaz <ahmetkorkmaz3453@gmail.com>
2021-12-11 15:01:39 +0300

    Fix project case resource

--- app/Http/Resources/ProjectCaseResource.php
+++ app/Http/Resources/ProjectCaseResource.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProjectCaseResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param Request $request
+     * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'order_no' => $this->order_no,
+            'project_id' => $this->project_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
