98ade7b510abed8e394ce3e7b9295237e7432a36
Ahmet Korkmaz <ahmetkorkmaz3453@gmail.com>
2021-12-11 21:23:40 +0300

    Add variables to environment resource

--- app/Http/Resources/EnvironmentResource.php
+++ app/Http/Resources/EnvironmentResource.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class EnvironmentResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param Request $request
+     * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'key' => $this->key,
+            'value' => $this->value,
+        ];
     }
 }
