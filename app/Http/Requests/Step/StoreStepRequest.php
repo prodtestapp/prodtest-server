c69ae931d9fb0933db889d7144488e3594452f6f
Ahmet Korkmaz <ahmetkorkmaz3453@gmail.com>
2022-01-06 20:16:00 +0300

    Fix enum usage

--- app/Http/Requests/Step/StoreStepRequest.php
+++ app/Http/Requests/Step/StoreStepRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests\Step;
 
+use App\Enums\BodyType;
+use App\Enums\HttpMethod;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -21,12 +23,12 @@ class StoreStepRequest extends FormRequest
                 'required',
                 'string',
                 'max:255',
-                Rule::in(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+                Rule::in(array_column(HttpMethod::cases(), 'value')),
             ],
             'headers' => ['nullable', 'string'],
             'body_type' => [
                 'nullable',
-                Rule::in(['json', 'raw', 'html', 'xml']),
+                Rule::in(array_column(BodyType::cases(), 'value')),
             ],
             'body' => ['nullable', 'string'],
             'expected_status' => ['nullable', 'string'],
