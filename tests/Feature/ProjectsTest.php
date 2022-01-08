2993e9228d1f072502ade5645141fef8da2d5e89
Ahmet Korkmaz <ahmetkorkmaz3453@gmail.com>
2022-01-08 14:37:39 +0300

    Add create project test

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+uses(DatabaseTransactions::class);
 
 it('Unauthorized projects list', function () {
     $response = $this->get('/api/projects');
@@ -13,4 +16,30 @@ it('Authorized projects list', function () {
     $response = $this->actingAs($user)->get('/api/projects');
 
     $response->assertStatus(200);
+});
+
+it('Create project', function () {
+    $user = User::factory()->create();
+
+    $response = $this->actingAs($user)->post('/api/projects', [
+        'name' => 'Test Project',
+    ]);
+
+    $response->assertStatus(201);
+    $response->assertJsonCount(1, 'data.environments');
+    $response->assertJsonStructure([
+        'data' => [
+            'id',
+            'name',
+            'background_color',
+            'cases_count',
+            'environments' => [
+                '*' => [
+                    'id',
+                ],
+            ],
+            'created_at',
+            'updated_at',
+        ],
+    ]);
 });
